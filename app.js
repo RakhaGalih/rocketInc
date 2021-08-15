675908494334637fa31e08365e5b180a21fe8512
RakhaGalih <54633534+RakhaGalih@users.noreply.github.com>
2021-08-15 09:45:30 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('menu').style.opacity = '0.9';
+}
+function closeMenu(){
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('menu').style.opacity = '0';
+}
